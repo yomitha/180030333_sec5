0c5946bc4aa158cf14783671b56575aa8080fa5d
yomitha <yomithaalapati@gmail.com>
2021-05-12 10:46:59 +0530

    practice of objects and classes

---
+++ WTN/src/session5/Calculator.java
@@ -0,0 +1,17 @@
+package session5;
+
+abstract  class Cal {
+	//Menu of methods
+	abstract int add(int a,int b);
+	abstract int sub(int a,int b);
+	abstract int mul(int a,int b);
+	abstract double div(int a,int b);
+
+}
+class Calculator
+{
+	public static void main(String args[])
+	{
+		
+	}
+}
