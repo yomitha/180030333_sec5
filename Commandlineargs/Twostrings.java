54555a58aad0e1b2600240704732a7edef589221
yomitha <65208804+yomitha@users.noreply.github.com>
2021-05-14 12:26:40 +0530

    practice of commandlineargs

--- Commandlineargs/Twostrings.java
+++ Commandlineargs/Twostrings.java
@@ -6,9 +6,7 @@ public class Twostrings {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println(args[0] + " technologies ");
-
-		  System.out.println(args[1]);
+		System.out.println(args[0] + " technologies " +args[1]);
 	}
 
 }
